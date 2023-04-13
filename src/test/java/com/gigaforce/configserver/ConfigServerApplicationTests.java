127f78726f8dad8302bcb7481ada12bb55b5761f
dk <dk@gmail.com>
2023-04-13 13:12:24 +0530

    initial commit and basic config added

---
+++ src/test/java/com/gigaforce/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigaforce.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
